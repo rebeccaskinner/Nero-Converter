6f42db64fa0fea9a38938d311ce9fc540c54be49
Tim Skinner <tim@timskinner.net>
2012-02-01 22:04:12 -0600

    initial commit

---
+++ converter.hs
@@ -0,0 +1,43 @@
+import Prelude hiding (catch)
+import Data.List
+import Control.Monad
+import System (getArgs)
+import System.Cmd
+import System.Directory
+import System.Exit
+import System.FilePath
+import System.IO.Error hiding (catch)
+import Control.Exception
+
+ultraIsoPath = "C:\\Program Files (x86)\\UltraISO\\UltraISO.exe"
+lamePath = "C:\\Users\\tim\\bin\\lame\\lame.exe" 
+
+dirChildren d = do
+    cont <- catch (getDirectoryContents d) permissionHandler
+    return (map (d</>) (filter visible cont))
+    where
+    visible p = '.' /= (head p)
+    permissionHandler e = if isPermissionError e
+                          then return []
+                          else ioError e
+
+getContentsRecursive top = do
+    children <- dirChildren top
+    subdirs  <- filterM doesDirectoryExist children
+    files    <- filterM fileFilter children
+    subFiles <- mapM getContentsRecursive subdirs
+    return $ files ++ concat subFiles
+    where
+    fileFilter f = fmap ((&&) ("nrg" `isSuffixOf` f)) $ doesFileExist f
+
+showHelp = do
+    putStrLn "converter: Convert nero audio disk images (.nrg) to mp3"
+    putStrLn "usage: convert.exe <input dir> <output dir>"
+
+-- main = fmap head getArgs >>= getContentsRecursive >>= mapM_ putStrLn
+main = do
+    (srcPath:dstPath:_) <- catch getArgs argsError 
+    getContentsRecursive srcPath >>= mapM_ putStrLn
+    where
+    argsError :: (Exception e) => e -> IO [String]
+    argsError e = showHelp >> exitFailure >> return []
